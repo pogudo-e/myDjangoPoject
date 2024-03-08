7b5d02b1ab3f51e57de7965abf8b51c421b0fc04
Pogudo Evgeny <sensesbit@gmail.com>
2024-03-09 00:40:04 +0300

    lesson9. Основы работы с моделями. CRUD. Часть 1

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 def index(response):
-    print(response)
-    return HttpResponse('hello world!')
+    news = News.objects.all()
+    res = '<h1>Список новостей</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+    return HttpResponse(res)
 
 
 def test(response):
